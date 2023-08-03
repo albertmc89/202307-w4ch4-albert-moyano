f998d4f7600fbfdd12d45f6d1aed1c42e06649a8
Albert Colubi <albertmc89@gmail.com>
2023-08-03 21:16:24 +0200

    Create App component and types file

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import NewSlothForm from "../NewSlothForm/NewSlothForm";
+
+const App = () => {
+  return (
+    <div className="container">
+      <NewSlothForm />
+    </div>
+  );
+};
+
+export default App;
